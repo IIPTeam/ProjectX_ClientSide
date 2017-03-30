d82222d1877c8389dec08e63682bebd9e09467ae
guobg <guobg1987@163.com>
2017-03-30 23:32:02 +0800

    Add staff id, password and logon component

--- ProjectX/js/logon/logon.js
+++ ProjectX/js/logon/logon.js
@@ -4,14 +4,16 @@ import {
     StyleSheet,
     Text,
     View,
-    TextInput
+    TextInput,
+    TouchableHighlight
 } from 'react-native';
 
 export default class Logon extends Component {
     render() {
         return (
             <View style={styles.container}>
-                    <Text style={styles.logonText}>Log On</Text>
+                <View style={styles.inputBox}>
+                    <Text style={styles.logonText}>Staff ID:</Text>
                     <TextInput
                         style={styles.userNameInput}
                         placeholder="Please enter username"
@@ -19,32 +21,82 @@ export default class Logon extends Component {
                         autoCapitalize="none"
                         autoCorrect={false}
                     />
+                </View>
+                <View style={styles.inputBox}>
+                    <Text style={styles.logonText}>Password:</Text>
+                    <TextInput
+                        style={styles.userNameInput}
+                        placeholder="Please enter password"
+                        underlineColorAndroid="transparent"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        secureTextEntry={true}
+                    />
+                </View>
+                <View style={styles.logonButtonBox}>
+                    <TouchableHighlight
+                        onPress={() => {
+                        }}
+                        activeOpacity={0.7}
+                        style={styles.logonButton}
+                        underlayColor='darkgreen'
+                    >
+                        <Text style={styles.logonButtonText}>Login</Text>
+                    </TouchableHighlight>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    logonButtonBox: {
+        flexDirection: 'row',
+    },
+    logonButton: {
+        backgroundColor: 'green',
+        flex: 1,
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    logonButtonText: {
+        fontSize: 20,
+        color: 'white'
+    },
+    inputBox: {
+        borderWidth: 2,
+        borderColor: 'green',
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginTop: 10,
+        marginBottom: 10,
+    },
     userNameInput: {
         flex: 1,
-        fontSize: 16,
-        padding: 4,
-        height: 40,
-        borderColor: 'black',
-        borderWidth: 1
+        fontSize: 20,
+        padding: 6,
+        justifyContent: 'center',
     },
     container: {
         flex: 1,
         justifyContent: 'center',
-        flexDirection:'row',
+        flexDirection: 'column',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
         padding: 20,
+        borderWidth: 1,
     },
     logonText: {
         padding: 5,
-        fontSize: 16,
-        marginRight: 10,
-        fontWeight: 'bold'
+        fontSize: 20,
+        marginRight: 5,
+        marginTop: 5,
+        fontWeight: 'bold',
+        color: 'green'
     }
 });
